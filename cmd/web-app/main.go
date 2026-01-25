6f5e0b4098a35fc67b8841fceafac7c4ff8cf978
AndrewHaine <andrew@andrewhaine.co.uk>
2026-01-25 13:15:54 +0000

    Add basic file server

---
+++ cmd/web-app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const (
+  staticDir = "cmd/web-app/public"
+  port = "8080"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./" + staticDir)))
+
+	fmt.Println("Starting web server on port " + port + "; serving files from " + staticDir)
+	err := http.ListenAndServe(":" + port, nil)
+
+	if err != nil {
+		fmt.Println("Error starting http server: " + err.Error())
+	}
+}
